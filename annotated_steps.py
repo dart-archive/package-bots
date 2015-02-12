ac7be91eaec7a67199f1c53bb01993c14bada49e
Rico Wind <ricow@google.com>
2015-02-12 15:40:46 +0100

    Revert "Don't use dart based pub server for now, seeing 500s"
    This reverts commit a5a1d36cfe0cb650b440f0b9656675ec1fe2b028.
    
    R=kustermann@google.com
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com//920823003

--- annotated_steps.py
+++ annotated_steps.py
@@ -135,6 +135,7 @@ def RunProcess(command, shell=False, extra_env=None):
   """
   env = dict(os.environ)
   env['TERM'] = 'nocolor'
+  env['PUB_HOSTED_URL'] = 'https://buildbot-dart-dot-dartlang-pub.appspot.com'
   del env['GIT_USER_AGENT']
   if extra_env:
     env.update(extra_env)
